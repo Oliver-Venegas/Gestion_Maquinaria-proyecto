7dc0639b06be69d609440831ae35e0473d21ae68
Oliver-Venegas <olivervenegas@hotmail.com>
2023-09-25 23:07:31 -0300

    Creacion del Log in
    Se creo un diseño de Log in capaz de ajustarse a tamaños de distintos tipos

--- passw_rec.php
+++ passw_rec.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Gestion de Maquinaria</title>
+    <title>Bodega Minera</title>
     <!-- CONEXION CSS -->
     <link rel="stylesheet" href="estilos.css">
 </head>
@@ -16,33 +15,36 @@
 
             <div class="col bg-white p-5 rounded-end">
 
-                <h2 class="fw-bold text-center py-4">Bienvenido</h2>
+                <h2 class="fw-bold text-center py-4">Cambiar la Contraseña</h2>
 
                 <form action="#">
                     <div class="mb-4">
-                        <label for="email" class="form-label">Correo Electronico</label>
-                        <input type="email" class="form-control" name="email_user">
+                        <label for="text" class="form-label">RUT</label>
+                        <input type="text" class="form-control" name="rut_renew">
                     </div>
 
                     <div class="mb-4">
-                        <label for="password" class="form-label">Contraseña</label>
-                        <input type="password" class="form-control" name="password_user">
+                        <label for="email" class="form-label">Correo Electronico</label>
+                        <input type="email" class="form-control" name="email_renew">
                     </div>
 <br>
-                   <div class="d-grid">
-                    <button type="submit" class="btn btn-primary">Ingresar</button>
+                    <div class="row align-items-stretch">
+                    <div class="col p-2">
+                   <button class="btn btn-light btn-lg"><a class="btn_backlog" href="index.html"> Regresar</a></button>
                    </div>
 
-                   <div class="my-3">
-                    <span><a href="passw_rec.php">Recuperar Contraseña</a></span>
+                   <div class="col-3 p-2">
+                    <button type="submit" class="btn btn-primary btn-lg">Guardar</button>
                    </div>
+                    </div>
+                
 
                 </form>
                 
             </div>
         </div>
     </div>
-
+ 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
